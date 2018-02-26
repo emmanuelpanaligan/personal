effbb298e870ee37203162d14c8b67e55cf942f6
Emmanuel Panaligan <panalige@gmail.com>
2018-02-26 14:10:20 -0500

    reload pdf

--- backend.js
+++ backend.js
@@ -1,5 +1,5 @@
 function show(shown, hidden) {
-	        $("#Embed").replaceWith($("#Embed"));
+	        document.getElementById("Embed").innerHTML = '<embed id="Embed" src="resume.pdf" type="application/pdf" width="100%" />';
 	        document.getElementById("Embed").style.height = ($(window).height()-70)+"px";
 			document.getElementById(shown).style.display='block';
 			document.getElementById(hidden).style.display='none';
