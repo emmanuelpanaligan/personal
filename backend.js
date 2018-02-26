3a0851e7786e72945fb4dd7679722b2addef2314
Emmanuel Panaligan <panalige@gmail.com>
2018-02-26 13:37:09 -0500

    formatting updates

--- backend.js
+++ backend.js
@@ -1,5 +1,5 @@
 function show(shown, hidden) {
-	        document.getElementById("Embed").style.height = ($(window).height()-10)+"px";
+	        document.getElementById("Embed").style.height = ($(window).height()-60)+"px";
 			document.getElementById(shown).style.display='block';
 			document.getElementById(hidden).style.display='none';
 			return false;
