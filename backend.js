9ecf5aeb2433ceb82e730fc987c520222174a859
Emmanuel Panaligan <panalige@gmail.com>
2018-02-27 17:11:47 -0500

    added Contact contents

--- backend.js
+++ backend.js
@@ -1,6 +1,19 @@
-function show(shown, hidden) {
-			document.getElementById(shown).style.display='block';
-			document.getElementById(hidden).style.display='none';
-		    document.getElementById("Resume").innerHTML = '<embed id="Embed" src="resume.pdf" type="application/pdf" width="100%" />';
-	        document.getElementById("Embed").style.height = ($(window).height()-70)+"px";
-		}
+function show(shown) {
+	document.getElementById("Resume").style.display='none';
+	document.getElementById("Splash").style.display='none';
+	document.getElementById("Contact").style.display='none';
+
+	switch (shown) {
+		case "Resume":
+		document.getElementById(shown).style.display='block';
+		document.getElementById("Resume").innerHTML = '<embed id="Embed" src="resume.pdf" type="application/pdf" width="100%" />';
+		document.getElementById("Embed").style.height = ($(window).height()-70)+"px";
+		break;
+		case "Splash":
+		document.getElementById(shown).style.display='block';
+		break;
+		case "Contact":
+		document.getElementById(shown).style.display='block';
+		break;
+	}
+}
